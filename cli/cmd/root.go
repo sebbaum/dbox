4a6e56ba9ef3621e7df3ba3b0db8ad455f8e45ed
Sebastian Baum <sebastian.baum@ion2s.com>
2021-08-14 20:20:06 +0200

    Edit basic information

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -11,14 +11,12 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "sebbaum.de",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "cboxctl",
+	Short: "Control your cbox local development environment",
+	Long: `With cboxctl you have full control over your local development environment
+for web applications.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Cbox is based on docker and docker-compose.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
