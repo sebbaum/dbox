181339b4a88ca7de056a84da7b83c70c473ff26a
Sebastian Baum <sebastian.baum@ion2s.com>
2021-08-14 20:50:39 +0200

    Implemented the down command

---
+++ cli/cmd/down.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// downCmd represents the down command
+var downCmd = &cobra.Command{
+	Use:   "down",
+	Short: "Shut down the environment",
+	Long:  `Runs the docker-compose down command for you in order to shut down the local development environment.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		call := exec.Command("docker-compose", "down")
+		call.Stdout = os.Stdout
+		call.Stderr = os.Stderr
+		err := call.Run()
+		if err != nil {
+			log.Fatalf("cmd.Run() failed with %s\n", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(downCmd)
+}
--- cli/cmd/up.go
+++ cli/cmd/up.go
@@ -12,7 +12,7 @@ import (
 var upCmd = &cobra.Command{
 	Use:   "up",
 	Short: "Start up the environment",
-	Long:  `Runs the docker-compose up command for you in order to start up the local development environment`,
+	Long:  `Runs the docker-compose up command for you in order to start up the local development environment.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		call := exec.Command("docker-compose", "up", "-d")
 		call.Stdout = os.Stdout
